bbc2fb770007b05795f2abdacd24dfc88890ae7d
Driftwoodye <silveira.mig@pm.me>
2026-09-15 14:20:16 +0200

    Remove temporary helper

--- about-patch.py
+++
@@ -1,5 +0,0 @@
-from pathlib import Path
-p=Path('index.html')
-s=p.read_text()
-s=s.replace('<div class="tabs"><button id="plantsBtn" class="tab active">Plants</button>','<div class="tabs"><button id="aboutBtn" class="tab active">About</button><button id="plantsBtn" class="tab">Plants</button>',1)
-p.write_text(s)
